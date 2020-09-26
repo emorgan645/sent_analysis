3ded24ba525c5859a9e0cc9ab1664688bf041535
emorgan645 <60742706+emorgan645@users.noreply.github.com>
2020-09-26 18:44:14 +0100

    Add files via upload

--- config.py
+++ config.py
@@ -4,10 +4,18 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     POSTS_PER_PAGE = 5
+
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+
+    RECAPTCHA_USE_SSL = False
+    RECAPTCHA_PUBLIC_KEY = '6LeKycsZAAAAAPVjWqlPh3nyZdAnKBmyx-Uxvb_I'
+    RECAPTCHA_PRIVATE_KEY = '6LeKycsZAAAAAH1_gNE_IJES32lT63WJydcxkF3y'
+    RECAPTCHA_OPTIONS = {'theme': 'white'}
--- tests.py
+++ tests.py
@@ -1,10 +1,10 @@
 from datetime import datetime, timedelta
 import unittest
 from app import app, db
-from app.models import User, Post
+from app.models import User, Search
 
 
-class UserModelCase(unittest.TestCase):
+class UserTest(unittest.TestCase):
     def setUp(self):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
         db.create_all()
@@ -26,46 +26,50 @@ class UserModelCase(unittest.TestCase):
                                          '?d=identicon&s=128'))
 
     def test_follow(self):
-        u1 = User(username='john', email='john@example.com')
-        u2 = User(username='susan', email='susan@example.com')
+        u1 = User(username='Test1', email='test1@example.com')
+        u2 = User(username='Test2', email='test2@example.com')
         db.session.add(u1)
         db.session.add(u2)
         db.session.commit()
+
         self.assertEqual(u1.followed.all(), [])
         self.assertEqual(u1.followers.all(), [])
 
         u1.follow(u2)
         db.session.commit()
+
         self.assertTrue(u1.is_following(u2))
+
         self.assertEqual(u1.followed.count(), 1)
-        self.assertEqual(u1.followed.first().username, 'susan')
+        self.assertEqual(u1.followed.first().username, 'Test2')
         self.assertEqual(u2.followers.count(), 1)
-        self.assertEqual(u2.followers.first().username, 'john')
+        self.assertEqual(u2.followers.first().username, 'Test1')
 
         u1.unfollow(u2)
         db.session.commit()
         self.assertFalse(u1.is_following(u2))
+
         self.assertEqual(u1.followed.count(), 0)
         self.assertEqual(u2.followers.count(), 0)
 
-    def test_follow_posts(self):
+    def test_follow_searches(self):
         # create four users
-        u1 = User(username='john', email='john@example.com')
-        u2 = User(username='susan', email='susan@example.com')
-        u3 = User(username='mary', email='mary@example.com')
-        u4 = User(username='david', email='david@example.com')
+        u1 = User(username='Test1', email='test1@example.com')
+        u2 = User(username='Test2', email='test2@example.com')
+        u3 = User(username='Test3', email='test3@example.com')
+        u4 = User(username='Test4', email='test4@example.com')
         db.session.add_all([u1, u2, u3, u4])
 
         # create four posts
         now = datetime.utcnow()
-        p1 = Post(body="post from john", author=u1,
-                  timestamp=now + timedelta(seconds=1))
-        p2 = Post(body="post from susan", author=u2,
-                  timestamp=now + timedelta(seconds=4))
-        p3 = Post(body="post from mary", author=u3,
-                  timestamp=now + timedelta(seconds=3))
-        p4 = Post(body="post from david", author=u4,
-                  timestamp=now + timedelta(seconds=2))
+        p1 = Search(keyword="test1", author=u1,
+                    timestamp=now + timedelta(seconds=1))
+        p2 = Search(keyword="test2", author=u2,
+                    timestamp=now + timedelta(seconds=4))
+        p3 = Search(keyword="test3", author=u3,
+                    timestamp=now + timedelta(seconds=3))
+        p4 = Search(keyword="test4", author=u4,
+                    timestamp=now + timedelta(seconds=2))
         db.session.add_all([p1, p2, p3, p4])
         db.session.commit()
 
@@ -76,15 +80,17 @@ class UserModelCase(unittest.TestCase):
         u3.follow(u4)  # mary follows david
         db.session.commit()
 
-        # check the followed posts of each user
-        f1 = u1.followed_posts().all()
-        f2 = u2.followed_posts().all()
-        f3 = u3.followed_posts().all()
-        f4 = u4.followed_posts().all()
+        # check the followed searches of each user
+        f1 = u1.followed_searches().all()
+        f2 = u2.followed_searches().all()
+        f3 = u3.followed_searches().all()
+        f4 = u4.followed_searches().all()
+
         self.assertEqual(f1, [p2, p4, p1])
         self.assertEqual(f2, [p2, p3])
         self.assertEqual(f3, [p3, p4])
         self.assertEqual(f4, [p4])
+        self.assertIsNot(f1, [p2, p3])
 
 
 if __name__ == '__main__':
